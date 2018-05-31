8280a9005b80048a5e63e182b37dc7d2874e07bf
Alex Booker <booker@booker.codes>
2018-05-31 18:40:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Electron Desktop Starter Template</h1>
+      </div>
+    )
+  }
+}
+
+export default App
